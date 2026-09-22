2c3e8e17d7c3055bc0c38e4303d97aaf1fcc776a
Tran Viet Thinh <thinhkesat@gmail.com>
2026-09-22 15:02:49 +0700

    Resolve Exercise level 1 and 2

---
+++ 02_Day_Variables_builtin_functions/day_2/variables.py
@@ -0,0 +1,72 @@
+#Exercise Level 1: 
+
+first_name = 'Tran'
+last_name = 'Thinh'
+country = 'Viet Nam'
+city = 'Ha Noi'
+age = 23
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+skills = ['HTML', 'CSS', 'JavaScript', 'Python']
+
+
+#Exercise Level 2:
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(skills))
+
+print(len(first_name))
+
+numone = 5
+numtwo = 4
+sum = numone + numtwo
+print('Sum of', numone, 'and', numtwo, 'is', sum)
+
+diff = numone - numtwo
+print('Difference of', numone, 'and', numtwo, 'is', diff)
+
+product = numone * numtwo
+print('Product of', numone, 'and', numtwo, 'is', product)
+
+division = numone / numtwo
+print('Division of', numone, 'and', numtwo, 'is', division)
+
+remainder = numone % numtwo
+print('Remainder of', numone, 'and', numtwo, 'is', remainder)
+
+exp = numone ** numtwo
+print('Exponent of', numone, 'and', numtwo, 'is', exp)
+
+floor_division = numone // numtwo
+print('Floor division of', numone, 'and', numtwo, 'is', floor_division)
+
+radius = 30
+area_of_circle = 3.14 * radius ** 2
+print('Area of circle with radius', radius, 'is', area_of_circle)
+
+circum_of_circle = 2 * 3.14 * radius
+print('Circumference of circle with radius', radius, 'is', circum_of_circle)
+
+print('Enter your first name:')
+first_name = input()
+
+print('Enter your last name:')
+last_name = input()
+
+print('Enter your country:')
+country = input()
+
+print('Enter your age:')
+age = input()
+
+
+
