3c155a956c95e7d73af45d5b5a8079155467200e
Tran Viet Thinh <thinhkesat@gmail.com>
2026-09-22 16:13:23 +0700

    doc(vi): translate Day 01 - Introduction and Day 02 - Variables & Built-in Functions to Vietnamese

---
+++ VietNam/01_Day_Introduction/helloworld.py
@@ -0,0 +1,25 @@
+# Lời Mở Đầu
+# Ngày 1 - 30 ngày thử thách với Python
+
+print("Hello World!")   # "Hello World!"
+
+print(2 + 3)   # Phép cộng (+)
+print(3 - 1)   # Phép trừ (-)
+print(2 * 3)   # Phép nhân (*)
+print(3 + 2)   # Phép cộng (+)
+print(3 - 2)   # Phép trừ (-)
+print(3 * 2)   # Phép nhân (*)
+print(3 / 2)   # Phép chia (/)
+print(3 ** 2)  # Lũy thừa (**)
+print(3 % 2)   # Phép chia lấy dư (%)
+print(3 // 2)  # Phép chia lấy phần nguyên (//)
+
+# Kiểm tra kiểu dữ liệu của các giá trị
+
+print(type(10))                  # số nguyên (int)
+print(type(3.14))                # số thực (float)
+print(type(1 + 3j))              # số phức (complex)
+print(type('Asabeneh'))          # chuỗi (str)
+print(type([1, 2, 3]))           # danh sách (list)
+print(type({'name': 'Asabeneh'}))  # từ điển (dict)
+print(type({9.8, 3.14, 2.7}))    # tập hợp (set)
